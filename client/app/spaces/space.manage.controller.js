d90f10af8e6390dc2dfb9e77123de2ec0eb19a76
Kevin <kev.k.wu@gmail.com>
2016-04-05 10:42:00 -0400

    added saving forms but weird errors

--- client/app/spaces/space.manage.controller.js
+++ client/app/spaces/space.manage.controller.js
@@ -18,13 +18,40 @@ class SpaceManageController {
       var results = (funcNameRegex).exec((this).constructor.toString());
       return (results && results.length > 1) ? results[1] : "";
     };
-    console.log(this.currentSpace.getName());
+    //this.testNum = 123;
   }
 
   addBeacon(){
+    if(this.currentSpace.beacons.indexOf(null) == -1)
+      this.currentSpace.beacons.push(null);
+  }
+  deleteBeacon(beacon){
+    console.log("test");
+    if(this.currentSpace.beacons.indexOf(beacon) !== -1){
+      this.currentSpace.beacons.splice(this.currentSpace.beacons.indexOf(beacon), 1);
+    }
 
   }
-
+  saveSpace() {
+    if (this.currentSpace) {
+      // Save it
+      this.APIClient.updateSpace(this.currentSpace)
+        .then(() => {
+        // Show toast
+        Materialize.toast('Your Space has been updated!', 4000);
+      // Clear input fields
+      this.newSpaceName = '';
+      this.newSpaceDescription = '';
+      this.newSpaceBeaconID = '';
+    }, error => {
+        Materialize.toast('Shit, an error', 4000);
+        console.log(error);
+      });
+      // TODO: figure out what beacon ID we will be putting in here!
+      // In order for mongoose to save the object the string inputted in the form
+      // needs to be of the right format of Schema.Types.ObjectId
+    }
+  }
 }
 // end class
 
