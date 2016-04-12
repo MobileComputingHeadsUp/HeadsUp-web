423ee95513a4b048e0895ca2be8d522ee55d5680
Kevin <kev.k.wu@gmail.com>
2016-04-12 11:16:51 -0400

    added dropdowns saving not working

--- client/app/spaces/space.manage.controller.js
+++ client/app/spaces/space.manage.controller.js
@@ -15,6 +15,7 @@ class SpaceManageController {
     this.$stateParams.id = this.currentSpace._id;
     //this.testNum = 123;
     //console.log(this.currentSpace.beacons);
+    console.log(this.currentSpace);
   }
 
   addBeacon(){
@@ -33,6 +34,7 @@ class SpaceManageController {
 
   }
   saveSpace() {
+    this.cleanUpOptionsArray();
     if (this.currentSpace) {
       // Save it
       this.APIClient.updateSpace(this.currentSpace)
@@ -49,6 +51,50 @@ class SpaceManageController {
       // In order for mongoose to save the object the string inputted in the form
       // needs to be of the right format of Schema.Types.ObjectId
     }
+  }
+  newDropDown() {
+    // the number'th dropdown
+    var number = this.currentSpace.requriedUserInfo.dropdown.length + 1;
+    var dropDown = {
+      number: number,
+      label: 'My Custom Dropdown',
+      editable: false,
+      optionStrings: ['default option 1', 'default option 2'],
+      // TODO: add in Angular Matieral in order to use a nice material switch to
+      // toggle this. Materialize doesnt have the switch :(
+      matchUsers: true
+    };
+    this.currentSpace.requriedUserInfo.dropdown.push(dropDown);
+    console.log(this.currentSpace.requriedUserInfo.dropdown);
+  }
+
+  setDropdownEditable(dropdown) {
+    dropdown.editable = true;
+  }
+  saveDropdown(dropdown) {
+    dropdown.editable = false;
+  }
+  newDropDownOption(dropdown) {
+    var count = dropdown.optionStrings.length+1;
+    dropdown.optionStrings.push('default option ' + count);
+  }
+  // Function utilized to transform the drop down options array
+  // In order to display the options in HTML via Angular's ng-repeat,
+  // each dropdown option must be an object, not a raw string. Though, when
+  // we want to save the data to the database, we want an array of raw strings.
+  // This function does that :-)
+  cleanUpOptionsArray() {
+    let dropdowns = [];
+    for (var i = 0; i < this.currentSpace.requriedUserInfo.dropdown.length; ++i) {
+      let currentDropdown = this.currentSpace.requriedUserInfo.dropdown[i];
+      dropdowns.push(currentDropdown);
+      for (var j = 0; j < currentDropdown.optionStrings.length; ++j) {
+        let stringVal = currentDropdown.optionStrings[j];
+        dropdowns[i].optionStrings[j] = stringVal;
+      }
+    }
+    // set this.customDropDowns to our new, fixed dropdowns array
+    this.currentSpace.requriedUserInfo.dropdown = dropdowns;
   }
 }
 // end class
